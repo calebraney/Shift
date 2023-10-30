0c49cffd350a0e9d23c28b9354d72172706e9cfb
Caleb Raney <110679107+calebraney@users.noreply.github.com>
2023-10-30 13:32:32 -0400

    working angle animation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import SplitType from 'split-type';
 import Lenis from '@studio-freight/lenis';
+import { attr } from './utilities';
 
 document.addEventListener('DOMContentLoaded', function () {
+  // Comment out for production
+  console.log('Local Script Loaded');
+
   //////////////////////////////
   // Global Variables
   // define variable for global use
@@ -9,6 +13,9 @@ document.addEventListener('DOMContentLoaded', function () {
   const links = document.querySelectorAll('a');
   const pageWrapper = document.querySelector('.page-wrapper');
   const pageMain = document.querySelector('.main-wrapper');
+  const clipItems = document.querySelectorAll('[clip-parent]');
+  const CLIP_FRONT = '[clip-front]';
+  const CLIP_BACK = '[clip-back]';
   const LOAD_GRID = '.load_grid';
   const LOAD_SQUARES = '.load_grid-item';
   const TRANSITION_DURATION = 0.35;
@@ -39,8 +46,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
   //////////////////////////////
   //LENIS Smoothscroll
-
-  // GSAP ANIMATIONS
   gsap.registerPlugin(ScrollTrigger);
 
   const lenis = new Lenis({
@@ -84,6 +89,13 @@ document.addEventListener('DOMContentLoaded', function () {
   //////////////////////////////
   // GSAP Animations
 
+  //set global interaction defaults
+  gsap.defaults({
+    duration: 0.6,
+    ease: 'power1.out',
+  });
+
+  // Page Transition
   links.forEach(function (link) {
     link.addEventListener('click', function (e) {
       if (
@@ -93,7 +105,7 @@ document.addEventListener('DOMContentLoaded', function () {
       ) {
         e.preventDefault();
         let destination = this.getAttribute('href');
-        let durationMS = TRANSITION_DURATION * 1000 + 100;
+        let DURATION_MS = TRANSITION_DURATION * 1000 + 100;
         gsap.set(`${LOAD_GRID}`, { display: 'grid' });
         gsap.fromTo(
           `${LOAD_SQUARES}`,
@@ -110,10 +122,102 @@ document.addEventListener('DOMContentLoaded', function () {
         );
         setTimeout(function () {
           window.location = destination;
-        }, durationMS);
+        }, DURATION_MS);
       }
     });
   });
+
+  const scrollTL = function (item) {
+    // default GSAP options
+    const defaults = {
+      toggleActions: 'play none none none',
+      scrub: true,
+      start: 'top 90%',
+      end: 'top 75%',
+    };
+    //override defaults if an attribute is present and a valid type.
+    defaults.toggleActions = attr(defaults.toggleActions, item.getAttribute('gsap-toggle-actions'));
+    defaults.scrub = attr(defaults.scrub, item.getAttribute('gsap-scrub'));
+    defaults.start = attr(defaults.start, item.getAttribute('gsap-scroll-start'));
+    defaults.end = attr(defaults.end, item.getAttribute('gsap-scroll-end'));
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: item,
+        start: defaults.start,
+        end: defaults.end,
+        toggleActions: defaults.scrub ? 'none none none none' : defaults.toggleActions,
+        scrub: defaults.scrub ? 0.5 : false,
+      },
+    });
+    return tl;
+  };
+
+  // Clip Hover Animation
+  clipItems.forEach(function (clipItem) {
+    let clipBack = clipItem.querySelector(CLIP_BACK);
+    let clipFront = clipItem.querySelector(CLIP_FRONT);
+    //return if elements aren't fount
+    if (!clipBack || !clipFront) return;
+    // set duration
+    let DURATION = attr(300, clipItem.getAttribute('clip-duration'));
+    let DURATION_MS = DURATION / 1000;
+    console.log(DURATION, DURATION_MS);
+
+    let tl = gsap.timeline({
+      paused: true,
+      ease: 'power3.out',
+      defaults: { duration: DURATION_MS * 0.7, ease: 'none' },
+    });
+    tl.set(clipBack, { opacity: 1 });
+    // step 1
+    tl.fromTo(
+      clipFront,
+      { clipPath: 'polygon(100% 0%, 100% 0%, 100% 100%, 0% 100%, 0% 0%)' },
+      {
+        clipPath: 'polygon(100% 0%, 100% 0%, 80% 100%, 0% 100%, 0% 0%)',
+        duration: DURATION_MS * 0.1,
+      }
+    );
+    //Step 2
+    tl.to(clipFront, { clipPath: 'polygon(20% 0%, 20% 0%, 0% 100%, 0% 100%, 0% 0%)' });
+    // tl.fromTo(
+    //   clipBack,
+    //   { clipPath: 'polygon(100% 100%, 100% 100%, 100% 100%, 100% 100%, 100% 100%)' },
+    //   { clipPath: 'polygon(100% 0%, 100% 100%, 0% 100%, 0% 100%, 100% 0%)' },
+    //   '<'
+    // );
+    //final step
+    tl.to(clipFront, {
+      clipPath: 'polygon(0% 0%, 0% 0%, 0% 0%, 0% 100%, 0% 0%)',
+      duration: DURATION_MS * 0.1,
+    });
+    // tl.to(clipBack, { clipPath: 'polygon(100% 0%, 100% 100%, 0% 100%, 0% 0%, 0% 0%)' }, '<');
+
+    clipItem.addEventListener('mouseenter', function () {
+      tl.play();
+    });
+
+    clipItem.addEventListener('mouseleave', function () {
+      tl.reverse();
+    });
+  });
+
+  /*
+      tl.fromTo(
+      clipFront,
+      { clipPath: 'polygon(100% 0%, 100% 100%, 100% 100%, 0% 100%, 0% 0%)' },
+      { clipPath: 'polygon(100% 0%, 100% 0%, 0% 100%, 0% 100%, 0% 0%)' }
+    );
+    tl.fromTo(
+      clipBack,
+      { clipPath: 'polygon(100% 100%, 100% 100%, 100% 100%, 100% 100%, 100% 100%)' },
+      { clipPath: 'polygon(100% 0%, 100% 100%, 0% 100%, 0% 100%, 100% 0%)' },
+      '<'
+    );
+    //final step
+    tl.to(clipFront, { clipPath: 'polygon(0% 0%, 0% 0%, 0% 0%, 0% 0%, 0% 0%)' });
+    tl.to(clipBack, { clipPath: 'polygon(100% 0%, 100% 100%, 0% 100%, 0% 0%, 0% 0%)' }, '<');
+  */
 
   //split text utility
   const runSplit = function (text) {
